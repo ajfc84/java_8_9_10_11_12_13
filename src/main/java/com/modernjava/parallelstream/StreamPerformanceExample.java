6c996f67952095a6651fec216f355eb30b2b9a9f
b01t <b01t.mymail@gmail.com>
2020-11-08 11:20:12 -0300

    Java 8 Parallel Streams

---
+++ src/main/java/com/modernjava/parallelstream/StreamPerformanceExample.java
@@ -0,0 +1,27 @@
+package com.modernjava.parallelstream;
+
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
+public class StreamPerformanceExample {
+    public static void main(String[] args) {
+        int loop = 20;
+        long result = measurePerformance(StreamPerformanceExample::sumSequentialStream, loop);
+        System.out.println("Sequential: " + result + " msecs");
+        result = measurePerformance(StreamPerformanceExample::sumParallelStream, loop);
+        System.out.println("Parallel: " + result + " msecs");
+    }
+
+    public static long measurePerformance(Supplier<Integer> supplier, int numberOfTimes) {
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i < numberOfTimes; i++)
+            supplier.get();
+        return System.currentTimeMillis() - startTime;
+    }
+    public static int sumSequentialStream() {
+        return IntStream.rangeClosed(0, 50000).sum();
+    }
+    public static int sumParallelStream() {
+        return IntStream.rangeClosed(0, 50000).parallel().sum();
+    }
+}
